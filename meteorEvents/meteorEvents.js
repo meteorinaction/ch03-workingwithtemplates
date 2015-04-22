a022709fddd2369540a7867dd8b70c005af61f5c
Stephan Hochhaus <stephan@yauh.de>
2015-04-22 20:28:40 +0200

    Listing 3.17 Event map for layout template

--- meteorEvents/meteorEvents.js
+++ meteorEvents/meteorEvents.js
@@ -1,5 +1,12 @@
 if (Meteor.isClient) {
-
+  Template.layout.events({
+    'click button': function (event, template) {
+      $('body').css('background-color', 'red');
+    },
+    'mouseenter #redButton': function (event, template) {
+      // start fancy animation
+    }
+  });
 }
 
 if (Meteor.isServer) {
