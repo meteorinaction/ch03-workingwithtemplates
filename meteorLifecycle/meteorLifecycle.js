2168c287844d1d501e8c7069008e89a2c80e2088
Stephan Hochhaus <stephan@yauh.de>
2015-04-22 20:39:57 +0200

    Listing 3.22 The template life-cycle callbacks

--- meteorLifecycle/meteorLifecycle.js
+++ meteorLifecycle/meteorLifecycle.js
@@ -1,4 +1,32 @@
 if (Meteor.isClient) {
+  Template.profile.onCreated(function () {
+    this.lastCallback = 'created';
+    console.log('profile.created', this);
+  });
+  Template.profile.onRendered(function () {
+    this.lastCallback = 'rendered';
+    console.log('profile.rendered', this);
+  });
+  Template.profile.onDestroyed(function () {
+    this.lastCallback = 'destroyed';
+    console.log('profile.destroyed', this);
+  });
+
+  Template.profile.helpers({
+    placeholder: function () {
+      console.log('profile.placeholder', this);
+      console.log('profile.tplInstance',
+        Template.instance().lastCallback);
+      return 'This is the {{placeholder}} helper';
+    }
+  });
+  Template.profile.events({
+    'click button': function (event, template) {
+      Template.instance().lastCallback = 'rendered and clicked';
+      console.log('profile.clicked', this);
+      console.log('profile.clicked.tplInstance', template);
+    }
+  });
 
 }
 
